e732e79e7312c0c93ff32586fc797694f87c779a
nohe-3 <wmh-8611@outlook.jp>
2025-12-23 16:48:37 +0900

    変更2

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+const __dirname = process.cwd();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+export default app;
